3c0ed89c35a9497d6dd7ad6d0168e3cf432b9077
atharvParlikar <58113282+atharvParlikar@users.noreply.github.com>
2020-05-01 11:31:43 +0530

    updated main.py for new file saving system.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import encryption
 import random_password_gen
 import random
 import pdb
+import json
+
 
 print('''
 
@@ -20,26 +22,34 @@ print('''
 ╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝
 
 ''')
-web_file = open('websites.txt', 'a')
-pass_file = open('passwords.txt', 'a')
-
-choice = int(input('1]store password\n2]get stored password\n>>>'))
-if choice == 1:
-    choice = int(input('1]store password of an existing account\n2]get a random password for a new account\n>>>'))
+with open('user_data.json', 'r+') as file:
+    choice = int(input('1]store passwords\n2]get stored passwords\n>>>'))
     if choice == 1:
-        website = input('website: ')
-        existing_password = input('password: ')
-        web_file.write(website + '\n')
-        pass_file.write((encryption.encrypt(existing_password)).decode() + '\n')
-        print('password saved successfully!')
+        choice = int(input('1]store password for existing account\n2]get a random password for new account\n>>>'))
+        if choice == 1:
+            website = input('enter the website: ')
+            password = input('password: ')
+            password = (encryption.encrypt(password)).decode()
+            user_dict = {website: password}
+            with open('user_data.json', 'r+') as file:
+                data = json.load(file)
+                data.update(user_dict)
+                file.seek(0)
+                json.dump(data, file)
+        elif choice == 2:
+            website = input('enter the website lable: ')
+            password = (encryption.encrypt(random_password_gen.randomPassword(random.randint(15,20)))).decode()
+            user_dict = {website: password}
+            with open('user_data.json', 'r+') as file:
+                data = json.load(file)
+                data.update(user_dict)
+                file.seek(0)
+                json.dump(data, file)
+            print('Your password for ' + website + ' is ' + (encryption.decrypt(password.encode())).decode())
     elif choice == 2:
-        website = input('website: ')
-        random_password = random_password_gen.randomPassword(random.randint(15,20))
-elif choice == 2:
-    web_file = open('websites.txt', 'r')
-    pass_file = open('passwords.txt', 'r')
-    website_search = input('website: ')
-    iteration_counter = 0
-    for i in web_file:
-        if i == website_search:
-            print(i)
+        website_to_find = input('enter the website lable: ')
+        with open('user_data.json', 'r'):
+            user_data_dict = json.load(file)
+            password = user_data_dict[website_to_find]
+            password = password.encode()
+            print('The password for ' + website_to_find + ' is ' + (encryption.decrypt(password)).decode())
